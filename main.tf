a13bd9c6e988050e09797ac97f7dc6ece41f16c8
Babah Kingsley <babahcloud9@gmail.com>
2024-06-14 02:57:43 +0200

    adding provissioners

--- main.tf
+++ main.tf
@@ -1,108 +1,108 @@
 
-resource "aws_vpc" "primus-vpc" {
-  cidr_block = var.cidr_block
-}
+# resource "aws_vpc" "primus-vpc" {
+#   cidr_block = var.cidr_block
+# }
 
 
-resource "aws_subnet" "primus-public-subnet" {
-  vpc_id            = aws_vpc.primus-vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = var.av-zone-a
+# resource "aws_subnet" "primus-public-subnet" {
+#   vpc_id            = aws_vpc.primus-vpc.id
+#   cidr_block        = "10.0.1.0/24"
+#   availability_zone = var.av-zone-a
 
-  tags = {
-    Name = "primus-public-subnet"
-  }
-}
-
-
-resource "aws_subnet" "primus-private-subnet" {
-  vpc_id            = aws_vpc.primus-vpc.id
-  cidr_block        = "10.0.0.0/24"
-  availability_zone = var.av-zone-c
-
-  tags = {
-    Name = "primus-private-subnet"
-  }
-}
+#   tags = {
+#     Name = "primus-public-subnet"
+#   }
+# }
 
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.primus-vpc.id
+# resource "aws_subnet" "primus-private-subnet" {
+#   vpc_id            = aws_vpc.primus-vpc.id
+#   cidr_block        = "10.0.0.0/24"
+#   availability_zone = var.av-zone-c
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow-ssh"
-  }
-}
+#   tags = {
+#     Name = "primus-private-subnet"
+#   }
+# }
 
 
-resource "aws_internet_gateway" "primus-igw" {
-  vpc_id = aws_vpc.primus-vpc.id
+# resource "aws_security_group" "allow_ssh" {
+#   name        = "allow_ssh"
+#   description = "Allow TLS inbound traffic and all outbound traffic"
+#   vpc_id      = aws_vpc.primus-vpc.id
 
-  tags = {
-    Name = "primus-igw"
-  }
-}
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-resource "aws_route_table" "pub-rt" {
-  vpc_id = aws_vpc.primus-vpc.id
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.primus-igw.id
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   tags = {
+#     Name = "allow-ssh"
+#   }
+# }
 
 
-  tags = {
-    Name = "pub-rt"
-  }
-}
+# resource "aws_internet_gateway" "primus-igw" {
+#   vpc_id = aws_vpc.primus-vpc.id
 
-resource "aws_route_table_association" "pub-rt-assoc" {
-  subnet_id      = aws_subnet.primus-public-subnet.id
-  route_table_id = aws_route_table.pub-rt.id
-}
+#   tags = {
+#     Name = "primus-igw"
+#   }
+# }
 
-# change the ami ID in the variable 
-resource "aws_instance" "primus-web" {
-  ami                         = var.ami
-  instance_type               = var.instance_type
-  associate_public_ip_address = true
-  availability_zone           = var.av-zone-a
-  key_name                    = aws_key_pair.pl-keypair.key_name
-  vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
-  subnet_id                   = aws_subnet.primus-public-subnet.id
-  user_data                   = file(var.userdata)
+# resource "aws_route_table" "pub-rt" {
+#   vpc_id = aws_vpc.primus-vpc.id
 
-  tags = {
-    name = "primus-web"
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.primus-igw.id
+#   }
 
-  depends_on = [aws_security_group.allow_ssh]
 
-}
+#   tags = {
+#     Name = "pub-rt"
+#   }
+# }
+
+# resource "aws_route_table_association" "pub-rt-assoc" {
+#   subnet_id      = aws_subnet.primus-public-subnet.id
+#   route_table_id = aws_route_table.pub-rt.id
+# }
+
+# # change the ami ID in the variable 
+# resource "aws_instance" "primus-web" {
+#   ami                         = var.ami
+#   instance_type               = var.instance_type
+#   associate_public_ip_address = true
+#   availability_zone           = var.av-zone-a
+#   key_name                    = aws_key_pair.pl-keypair.key_name
+#   vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
+#   subnet_id                   = aws_subnet.primus-public-subnet.id
+#   user_data                   = file(var.userdata)
+
+#   tags = {
+#     name = "primus-web"
+#   }
+
+#   depends_on = [aws_security_group.allow_ssh]
+
+# }
 
 # you need to change the path to the public
 
@@ -139,27 +139,62 @@ data "aws_ami" "amazon-linux2" {
 resource "aws_instance" "data-ec2" {
   ami                    = data.aws_ami.amazon-linux2.id
   key_name               = aws_key_pair.pl-keypair.key_name
-  subnet_id              = aws_subnet.primus-public-subnet.id
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  # subnet_id              = aws_subnet.primus-public-subnet.id
+  # vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   instance_type          = var.instance_type
 
   tags = {
     Name = "data-ec2"
   }
-}
 
+  provisioner "file" {
+    source = "user-data.sh"
+    destination = "/tmp/user-data.sh"
 
-data "aws_vpc" "default" {
-  id = var.default-vpc_id
-}
-
-
-resource "aws_subnet" "data-subnet" {
-  vpc_id            = data.aws_vpc.default.id
-  cidr_block        = "172.31.64.0/20"
-  availability_zone = var.av-zone-c
-
-  tags = {
-    Name = "data-subnet"
+    connection {
+      type = "ssh"
+      user = "ec2-user"
+      private_key = file("~/.ssh/id_rsa")
+      host = "${self.public_ip}"
+    }
+    
   }
+
+  provisioner "remote-exec" {
+    inline = [ 
+      "chmod u+x /tmp/user-data.sh",
+      "./user-data.sh",
+      "mkdir Babah"
+     ]
+    connection {
+      type = "ssh"
+      user = "ec2-user"
+      private_key = file("~/.ssh/id_rsa")
+      host = "${self.public_ip}"
+    }
+
+}
+
+}
+
+
+# data "aws_vpc" "default" {
+#   id = var.default-vpc_id
+# }
+
+
+# resource "aws_subnet" "data-subnet" {
+#   vpc_id            = data.aws_vpc.default.id
+#   cidr_block        = "172.31.64.0/20"
+#   availability_zone = var.av-zone-c
+
+#   tags = {
+#     Name = "data-subnet"
+#   }
+# }
+
+
+output "ip" {
+  value = aws_instance.data-ec2.public_ip
+  
 }
